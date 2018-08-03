5cf56afb26af8cb89e125e59dc53b2ecefe61c3e
Ulani Qi <uhq@andrew.cmu.edu>
2018-08-03 13:28:16 -0400

    Commit run_simulation

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -16,3 +16,22 @@ model_select = function(covariates, responses, cutoff){
 
 gen = generate_data(10,10)
 model_select(gen$covariates, gen$responses, 0)
+
+run_simulation = function(n_trials = 1000, n = c(100, 1000, 10000), 
+                          p = c(10, 20, 50), cutoff = 0.05){
+  combs = expand.grid(n, p)
+  helper = function(n, p, cutoff){
+    data = generate_data(n, p)
+    result = model_select(data$covariates, data$responses, cutoff)
+    summ = summary(result)
+    return(summ$coefficients[-1,"Pr(>|t|)"])
+  }
+  for (i in 1:length(combs)){
+    trials = unlist(replicate(n_trials, helper(combs[i,1], combs[i,2], cutoff)))
+    png(paste('trial',i,'.png', sep=''))
+    hist(trials, xlab = "p-value", main = paste('Trial',i,sep=" "))
+    dev.off()   
+  }
+}
+
+run_simulation()
