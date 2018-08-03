ce6f29c9aa9795b2513ab01fc1ebbb0997dce622
Ulani Qi <uhq@andrew.cmu.edu>
2018-08-03 12:53:22 -0400

    Commit model_select

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -5,3 +5,14 @@ generate_data = function(n, p){
 }
 
 generate_data(2,3)
+
+model_select = function(covariates, responses, cutoff){
+  regr = lm(responses ~ covariates)
+  coeffs = coef(regr)[-1]
+  inds = which((coeffs <= cutoff) & (!is.na(coeffs)))
+  new.regr = lm(responses ~ covariates[, inds])
+  return(new.regr)
+}
+
+gen = generate_data(10,10)
+model_select(gen$covariates, gen$responses, 0)
